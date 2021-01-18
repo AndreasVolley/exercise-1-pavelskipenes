f9f11428b6baf19a49d93ef769c7f22740e6edcc
Pavel Skipenes <pavelgs@stud.ntnu.no>
2021-01-18 20:48:35 +0100

    completed assignment in go

--- go/foo.go
+++ go/foo.go
@@ -9,21 +9,28 @@ import (
 )
 
 var i = 0
+const ITERATIONS  = 1000000
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+    for a := 0; a < ITERATIONS; a++ {
+        i +=1
+     }
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+    for a := 0; a < ITERATIONS; a++ {
+        i -=1
+     }
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+	// Try doing the exercise both with and without it!
 
     // TODO: Spawn both functions as goroutines
-	
+    go incrementing()
+    go decrementing()
+
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(500*time.Millisecond)
